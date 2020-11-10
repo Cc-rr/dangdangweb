64fe1835579e2fad985d073b23f4b4dfc46a69c7
CC-rr <1607209159@qq.com>
2020-11-10 18:11:57 +0800

    轮播

--- src/components/Swiper/Swiper.vue
+++ src/components/Swiper/Swiper.vue
@@ -1,13 +1,81 @@
 <template>
-
+  <div class="swiper-container" ref="swiper">
+    <div class="swiper-wrapper">
+      <slot></slot>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+  </div>
 </template>
 
 <script>
-export default {
+import Swiper from 'swiper/swiper-bundle.js'
+import 'swiper/swiper-bundle.css'
 
+export default {
+  name: 'Swiper',
+  // 定义属性
+  props: {
+    autoplay: {
+      type: Number,
+      default: 0
+    },
+    loop: {
+      type: Boolean,
+      default: true
+    }
+  },
+  mounted () {
+    // 钩子函数  挂载使用
+    // 但是需要在这里挂载  consol.log(this.$refs.swiper)
+    var that = this
+    /* eslint-disable */
+    new Swiper(this.$refs.swiper, {      //当前拿到的是此时的div 还可以 this.$el   //原本是通过css选择器 .swiper-container去实例化  第二次也实例化一次
+      loop: this.loop, //是否进行循环轮播
+      autoplay: this.autoplay  //轮播事件
+        ? {
+            delay: this.autoplay,
+            stopOnLastSlide: false,
+            disableOnInteraction: true
+          }
+        : false,
+      pagination: {
+        el: ".swiper-pagination"
+      },
+      on: {
+        slideChangeTransitionEnd: function() {
+          // console.log(this.activeIndex)
+          // console.log(this.realIndex)
+          // 触发一个自定义事件 拿到真实下标
+          that.$emit("change", this.realIndex);
+        }
+      }
+    });
+    /* eslint-enable */
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.swiper-container {
+  width: 100%;
+  height: 180px;
+}
+</style>
+<style lang="scss">
+.swiper-pagination-bullet {
+  opacity: 1;
+  vertical-align: middle;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background-color: hsla(0, 0%, 100%, 0.7);
+}
 
+.swiper-pagination-bullet-active {
+  width: 20px;
+  height: 10px;
+  background: url("../../assets/icon/dot.png") no-repeat;
+  background-size: 100%;
+}
 </style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,28 +1,53 @@
 <template>
-  <div class="header">
-    <el-row :gutter="20">
-      <el-col :span="8"><a href="mine.html?cpid=0"><div class="grid-content bg-purple user-btn"></div></a></el-col>
-      <el-col :span="12"><div class="grid-content bg-purple"><div class="header-logo"></div></div></el-col>
-      <el-col :span="2"><div class="grid-content bg-purple"><div class="header-search"></div></div></el-col>
-    </el-row>
+  <div>
+    <div class="header">
+      <el-row :gutter="20">
+        <el-col :span="8"><a href="mine.html?cpid=0"><div class="grid-content bg-purple user-btn"></div></a></el-col>
+        <el-col :span="12"><div class="grid-content bg-purple"><div class="header-logo"></div></div></el-col>
+        <el-col :span="2"><div class="grid-content bg-purple"><div class="header-search"></div></div></el-col>
+      </el-row>
+    </div>
+    <Swiper :autoplay="2000" :loop="true" @change="changeHandler" ><!-- 也可以在这自定义组件写 ref ='xxx'   -->
+      <SwiperItem>1</SwiperItem>
+      <SwiperItem>2</SwiperItem>
+      <SwiperItem>3</SwiperItem>
+    </Swiper>
   </div>
-
 </template>
 
 <script>
+import { Swiper, SwiperItem } from '@/components/Swiper'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  methods: {
+    changeHandler (payload) {
+      console.log('index', payload)
+    }
+  },
+  // mounted(){
+  //   console.log(this.$refs.xxx.$el)  //拿到div<Swiper>
+  // }
+  created () {
+    // 下载数据一般放在这里 使用第三方请求数据方法
+    fetch('https://mhd.zhuishushenqi.com/comic_v2/getproad?apptype=8&appversion=1.0&channel=web-app&adgroupid=123').then(res => res.json()).then(res => { console.log(res) }) // 使.then转换数据
+  }
 }
+
 </script>
 
 <style lang='scss' scoped>
+@import "~@/assets/styles/mixins.scss";
   .header{
     max-width: 540PX;
     height: 100%;
     margin: 0 auto;
     // border: 1px solid red;
     padding-left: 20px;
-  }
+    @include border-bottom;
   .user-btn{
       width: 25px;
       height: 25px;
@@ -41,5 +66,5 @@ export default {
       background: url(~@/assets/icon/header-search.png);
       background-size: 100%;
     }
-
+  }
 </style>
